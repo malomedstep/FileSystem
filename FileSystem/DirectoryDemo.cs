6063ec945a013862470541b3292560155a4cfe93
Oleksandr Malomed <oleksandr.malomed@outlook.com>
2021-05-23 18:32:46 +0400

    Add project files.

---
+++ FileSystem/DirectoryDemo.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace FileSystem
+{
+    class DirectoryDemo
+    {
+        public static void Run()
+        {
+            // System.IO.Directory содержит только статические методы-обертки над обычными OS-specific функциями для работы с файловой системой
+            // касающиеся, в основном, директорий
+
+            // создает директорию и возвращает DirectoryInfo, описывающий эту директорию
+            _ = Directory.CreateDirectory("create_directory");
+
+            // возвращает список логических томов - C, D, E и т.д.
+            _ = Directory.GetLogicalDrives();
+
+            // а вот теперь начинается интересное:
+            // возвращает список директорий по конкретному пути
+            _ = Directory.GetDirectories(@"C:\");
+            // возвращает список файлов по конкретному пути
+            _ = Directory.GetFiles(@"C:\");
+            // возвращает список директорий и файлов по конкретному пути
+            _ = Directory.GetFileSystemEntries(@"C:\");
+
+            // возвращает путь к текущей директории - рабочей директории, в которой запущена программа
+            _ = Directory.GetCurrentDirectory();
+        }
+    }
+}
---
+++ FileSystem/FileDemo.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+
+namespace FileSystem
+{
+    class FileDemo
+    {
+        public static void Run()
+        {
+            // System.IO.File содержит только статические методы-обертки над обычными OS-specific функциями для работы с файловой системой
+            // касающие только файлов
+
+            // создает файл и возвращает FileStream для записи
+            _ = File.Create("file_create.txt");
+
+            // открывает файл и возвращает FileStream для чтения/записи - зависит от режима
+            _ = File.Open("file_create.txt", FileMode.Open);
+
+            // проверяет, существует ли указанный файл
+            _ = File.Exists("file.txt");
+
+            // File.WriteAllxxx и File.AppendAllxxx используются для быстрой [до]записи разных данных в файл в 1 строку
+            File.WriteAllText("write_all.txt", "salam");
+            File.AppendAllText("write_all.txt", "salam");
+
+            // File.ReadAllxxx используется для быстрого считывания разных данных в файл в 1 строку
+            _ = File.ReadAllText("write_all.txt");
+
+            // удаляет файл
+            File.Delete("file.txt");
+
+            // File.Move используется как для перемещения файла, так и для переименования
+            File.Move("a.txt", "b.txt");
+
+            // Используются для получения и изменения атрибутов файла
+            _ = File.GetAttributes("file.txt");
+            File.SetAttributes("file.txt", FileAttributes.Hidden | FileAttributes.ReadOnly);
+        }
+    }
+}
